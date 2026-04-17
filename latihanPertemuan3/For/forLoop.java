d0b67c5abf857cf959f1f66b29a3812b2752d2a2
rara <farahabiba987@gmail.com>
2026-04-17 15:15:19 +0700

    looping

--- latihanPertemuan3/For/forLoop.java
+++ latihanPertemuan3/For/forLoop.java
@@ -2,7 +2,7 @@ package latihanPertemuan3.For;
 
 public class forLoop {
     public static void main(String[] args) {
-        for (int kamsyiah = 0; kamsyiah < 10; kamsyiah++) {
+        for (int kamsyiah = 0; kamsyiah <=10; kamsyiah++) {
             System.out.println("kamsyiah: " + kamsyiah);
         }   
     }
--- latihanPertemuan3/For/latihanperUlangan.java
+++ latihanPertemuan3/For/latihanperUlangan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class latihanperUlangan {
 
     public static void main(String[] args) {
-        scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         /*
         1. Entri data
@@ -14,35 +14,43 @@ public class latihanperUlangan {
     */
     //variabel
     int menu, total = 0, harga=0, jumlah=0, iterasi=0;
+    String namaKegiatan = "0", namaBarang = "0", jumlahBarang = "0";
         
         //menu bisa diulang>> while/dowhile
         do{
             System.out.println("1. Entri Data");
             System.out.println("2. Print Data");
-            System.out.println("pilih menu : ");
+            System.out.print("pilih menu : ");
             menu = sc.nextInt();
-
+            
             //percabangan menu>>switch case
-            switch(menu){
-                case 1:
-                    System.out.println("=== Entri Data ===");
-                    //data diulang > 5x
-                    System.out.println("berapa banyak barang yang dibutuhan ? : ");
-                    iterasi = sc.nextInt();
-                    for (int i = 1; i <10; i++) {
-                        //input data
-                        System.out.println("Masukkan harga barang ke - " + i + " : ");
-                        break;
-                    }
-                      
-                    case 2:
-                        default:
-                        System.exit(0);
-                        break;
-                
-                    }
-                }
+        switch(menu){
+            case 1:
+                //entri data
+                System.out.println("=== Entri Data ===");
+                //data diulang > 5x
+                System.out.println("jumlah data yang akan diinputkan : ");
+                iterasi = sc.nextInt();
+                for (int i = 1; i <= iterasi; i++) {
+                    System.out.println();("Nama Kegiatan : ");
+                    namaKegiatan = sc.nextLine();
+
+                    System.out.println("Jumlah Barang : ");
+                    jumlahBarang = sc.nextInt();
+
+                    System.out.print("Harga Barang : ");
+                    harga = sc.nextInt();
+                    System.out.println("total += jumlahBarang * harga;");
+            break;
             }
+
+            case 2:
+                //exit
+                default:
+                System.exit(0);
+            break;
         }
-    }    
-} 
+        } while (true);
+    }
+}
+        
