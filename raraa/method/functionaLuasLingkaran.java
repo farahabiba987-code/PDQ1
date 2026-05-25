45de0a2e67b330327be9ab22f2fbf6fd22ca2231
rara <farahabiba987@gmail.com>
2026-05-25 14:33:46 +0700

    parameter

---
+++ raraa/method/functionaLuasLingkaran.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class functionaLuasLingkaran {
+    static double luasLingkaran(double r) {
+        double luas = 3.14 * r * r;
+        return luas; 
+    }
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan jari-jari lingkaran: ");
+        double r = sc.nextDouble();
+        
+        double luas = luasLingkaran(r);
+        System.out.println(luas);
+    }
+    
+}
